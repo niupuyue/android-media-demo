e85da460bb71f8c9d3d0e351d1e722c09b745f80
paulniu <niupuyue@aliyun.com>
2019-08-06 22:41:57 +0800

    添加textureview预览相机

--- 03-android-media-camera-video/app/src/main/java/com/paulniu/camera_video/TextureViewActivity.java
+++ 03-android-media-camera-video/app/src/main/java/com/paulniu/camera_video/TextureViewActivity.java
@@ -1,13 +1,260 @@
 package com.paulniu.camera_video;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.util.Size;
+import android.view.Surface;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * Coder: niupuyue
  * Date: 2019/8/6
  * Time: 18:34
- * Desc:
+ * Desc: 使用TextureViewActivity预览相机
  * Version:
  */
-public class TextureViewActivity extends AppCompatActivity {
+public class TextureViewActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private String cameraId;
+    private Size previewSize;
+    private HandlerThread handlerThread;
+    private Handler handler;
+    private CameraDevice cameraDevice;
+    private CaptureRequest request;
+    private CaptureRequest.Builder builder;
+    private CameraCaptureSession session;
+
+    private Button btn_texture_font, btn_texture_back;
+    private TextureView textureView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_textureview);
+
+        btn_texture_font = findViewById(R.id.btn_texture_font);
+        btn_texture_back = findViewById(R.id.btn_texture_back);
+
+        btn_texture_font.setOnClickListener(this);
+        btn_texture_back.setOnClickListener(this);
+
+        textureView = findViewById(R.id.textureView);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startCameraThread();
+        if (!textureView.isAvailable()) {
+            textureView.setSurfaceTextureListener(textureListener);
+        } else {
+            startPreView();
+        }
+    }
+
+    private TextureView.SurfaceTextureListener textureListener = new TextureView.SurfaceTextureListener() {
+        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+            // 当特效图review可用的时候，打开相机
+            setupCamera(width, height);
+            openCamera();
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+        }
+    };
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void setupCamera(int width, int height) {
+        // 获取Camera管理对象CameraManager
+        CameraManager manager = (CameraManager) getSystemService(CAMERA_SERVICE);
+        try {
+            // 遍历所有的摄像头
+            for (String cameraId : manager.getCameraIdList()) {
+                CameraCharacteristics cameraCharacteristics = manager.getCameraCharacteristics(cameraId);
+                Integer facing = cameraCharacteristics.get(CameraCharacteristics.LENS_FACING);
+                // 在此处默认打开后置摄像头
+                if (facing != null && facing == CameraCharacteristics.LENS_FACING_FRONT) {
+                    continue;
+                }
+                // 获取StreamConfigurationMap,他是管理所有摄像头支持的输出格式和尺寸
+                StreamConfigurationMap map = cameraCharacteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+                assert map != null;
+                previewSize = getOptimalSize(map.getOutputSizes(SurfaceTexture.class), width, height);
+                TextureViewActivity.this.cameraId = cameraId;
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    //选择sizeMap中大于并且最接近width和height的size
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private Size getOptimalSize(Size[] sizeMap, int width, int height) {
+        List<Size> sizeList = new ArrayList<>();
+        for (Size option : sizeMap) {
+            if (width > height) {
+                if (option.getWidth() > width && option.getHeight() > height) {
+                    sizeList.add(option);
+                }
+            } else {
+                if (option.getWidth() > height && option.getHeight() > width) {
+                    sizeList.add(option);
+                }
+            }
+        }
+        if (sizeList.size() > 0) {
+            return Collections.min(sizeList, new Comparator<Size>() {
+                @Override
+                public int compare(Size lhs, Size rhs) {
+                    return Long.signum(lhs.getWidth() * lhs.getHeight() - rhs.getWidth() * rhs.getHeight());
+                }
+            });
+        }
+        return sizeMap[0];
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void openCamera() {
+        CameraManager cameraManager = (CameraManager) getSystemService(CAMERA_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        try {
+            cameraManager.openCamera(cameraId, mStateCallback, handler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private CameraDevice.StateCallback mStateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(@NonNull CameraDevice camera) {
+            cameraDevice = camera;
+            startPreView();
+        }
+
+
+        @Override
+        public void onDisconnected(@NonNull CameraDevice camera) {
+            if (cameraDevice != null) {
+                cameraDevice.close();
+                camera.close();
+                cameraDevice = null;
+            }
+        }
+
+        @Override
+        public void onError(@NonNull CameraDevice camera, int error) {
+            if (cameraDevice != null) {
+                cameraDevice.close();
+                camera.close();
+                cameraDevice = null;
+            }
+        }
+    };
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void startPreView() {
+        SurfaceTexture mSurfaceTexture = textureView.getSurfaceTexture();
+        mSurfaceTexture.setDefaultBufferSize(previewSize.getWidth(), previewSize.getHeight());
+
+        Surface previewSurface = new Surface(mSurfaceTexture);
+        try {
+            builder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            builder.addTarget(previewSurface);
+            cameraDevice.createCaptureSession(Arrays.asList(previewSurface), new CameraCaptureSession.StateCallback() {
+                @Override
+                public void onConfigured(@NonNull CameraCaptureSession session) {
+                    request = builder.build();
+                    session = session;
+                    try {
+                        session.setRepeatingRequest(request, null, handler);
+                    } catch (CameraAccessException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+
+                }
+            }, handler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void startCameraThread() {
+        handlerThread = new HandlerThread("CameraHandlerThread");
+        handlerThread.start();
+        handler = new Handler(handlerThread.getLooper());
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (session != null) {
+            session.close();
+            session = null;
+        }
+        if (cameraDevice != null) {
+            cameraDevice.close();
+            cameraDevice = null;
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_texture_font:
+
+                break;
+            case R.id.btn_texture_back:
+
+                break;
+        }
+    }
 }
