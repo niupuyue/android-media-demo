92c35e7036d38b4b62521b86dac813390e7c5009
paulniu <niupuyue@renruihr.com>
2019-08-06 09:31:48 +0800

    code format

--- 01-android-media-create-image/app/src/main/java/com/paulniu/media/MainActivity.java
+++ 01-android-media-create-image/app/src/main/java/com/paulniu/media/MainActivity.java
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
         btn03.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+
             }
         });
     }
