ab8ef79ee5414337e2ba285bf79de7d3387e1f89
paulniu <niupuyue@aliyun.com>
2019-08-07 18:35:54 +0800

    code format

--- 03-android-media-camera-video/app/src/main/java/com/paulniu/camera_video/SurfaceViewActivity.java
+++ 03-android-media-camera-video/app/src/main/java/com/paulniu/camera_video/SurfaceViewActivity.java
@@ -57,6 +57,7 @@ public class SurfaceViewActivity extends AppCompatActivity implements View.OnCli
     private CaptureRequest.Builder mCaptureRequestBuilder;
     private CaptureRequest mCaptureRequest;
     private CameraCaptureSession session;
+    // 操作SurfaceView的对象，通过SurfaceHolder对象，我们可以设置SurfaceView的size，format等信息，同时还可以监听SurfaceView的变化
     private SurfaceHolder holder;
 
     private boolean isFontCamera = true;
@@ -104,6 +105,7 @@ public class SurfaceViewActivity extends AppCompatActivity implements View.OnCli
         @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
         @Override
         public void surfaceCreated(SurfaceHolder surfaceHolder) {
+            // 当SurfaceView第一次创建完成，根据surfaceHolder获取SurfaceView的宽高
             surfaceViewHolderWidth = surfaceHolder.getSurfaceFrame().width();
             surfaceViewHolderHeight = surfaceHolder.getSurfaceFrame().height();
         }
@@ -121,6 +123,7 @@ public class SurfaceViewActivity extends AppCompatActivity implements View.OnCli
 
     /**
      * 设置Camera的基本配置信息
+     * 如果需要切换摄像头则需要重新设置摄像头类型(或者可以在第一次使用的时候将摄像头信息保存下来，后面当需要使用不同呢方向的摄像头时直接赋值即可)
      *
      * @param width
      * @param height
@@ -149,6 +152,7 @@ public class SurfaceViewActivity extends AppCompatActivity implements View.OnCli
                 // 获取StreamConfigurationMap，他是管理摄像头支持的所有输出格式和尺寸
                 StreamConfigurationMap map = cameraCharacteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
                 assert map != null;
+                // 设置SurfaceView中可以预览摄像头的尺寸大小，一般情况下，我们会更具SurfaceView的大小进行限制，但是一般都是跟手机的屏幕最大宽度相同即可
                 mPreviewSize = getOptimalSize(map.getOutputSizes(SurfaceTexture.class), width, height);
                 mCameraId = cameraId;
             }
@@ -189,6 +193,7 @@ public class SurfaceViewActivity extends AppCompatActivity implements View.OnCli
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     private void openCamera() {
         CameraManager cameraManager = (CameraManager) getSystemService(CAMERA_SERVICE);
+        // 检查是否获取摄像头全向
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
@@ -207,12 +212,15 @@ public class SurfaceViewActivity extends AppCompatActivity implements View.OnCli
     private CameraDevice.StateCallback mCameraDeviceStateCallback = new CameraDevice.StateCallback() {
         @Override
         public void onOpened(CameraDevice cameraDevice) {
+            // 当摄像头被打开的时候，将当前的摄像头驱动对象赋值给cameraDevice
             SurfaceViewActivity.this.cameraDevice = cameraDevice;
+            // 开始预览摄像头
             startPreView();
         }
 
         @Override
         public void onDisconnected(CameraDevice cameraDevice) {
+            // 断开连接时需要将cameraDevice关闭，并设置为null
             if (SurfaceViewActivity.this.cameraDevice != null) {
                 SurfaceViewActivity.this.cameraDevice.close();
                 cameraDevice.close();
@@ -233,6 +241,8 @@ public class SurfaceViewActivity extends AppCompatActivity implements View.OnCli
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     private void startPreView() {
         try {
+            // Surface用途：用来处理由屏幕合成器管理的原始缓存区
+            // 通过holder
             Surface surface = holder.getSurface();
             mCaptureRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
             if (surface != null) {
