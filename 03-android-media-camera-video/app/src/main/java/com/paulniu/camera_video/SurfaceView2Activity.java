ad53ba507af79407eaf4e551d9b62716994a23e5
paulniu <niupuyue@aliyun.com>
2019-08-08 09:50:42 +0800

    添加第二种设置SurfaceView预览摄像头的方法，并且通过setPreviewCallback获取byte[]数据

--- 03-android-media-camera-video/app/src/main/java/com/paulniu/camera_video/SurfaceView2Activity.java
+++ 03-android-media-camera-video/app/src/main/java/com/paulniu/camera_video/SurfaceView2Activity.java
@@ -1,14 +1,22 @@
 package com.paulniu.camera_video;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageFormat;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.ByteArrayOutputStream;
 
 /**
  * Coder: niupuyue
@@ -23,6 +31,10 @@ public class SurfaceView2Activity extends AppCompatActivity {
 
     private Camera camera;
 
+    private Camera.Size mPreviewSize;
+
+    private ImageView icon;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +44,8 @@ public class SurfaceView2Activity extends AppCompatActivity {
 
         setContentView(R.layout.activity_surfaceview2);
 
+        icon = findViewById(R.id.icon);
+
         surface_view = findViewById(R.id.surface_view);
         surface_view.getHolder().addCallback(new SurfaceHolder.Callback() {
             @Override
@@ -39,7 +53,7 @@ public class SurfaceView2Activity extends AppCompatActivity {
                 try {
                     camera.setPreviewDisplay(holder);
                     camera.startPreview();
-                }catch (Exception ex){
+                } catch (Exception ex) {
                     ex.printStackTrace();
                 }
             }
@@ -59,5 +73,36 @@ public class SurfaceView2Activity extends AppCompatActivity {
         camera = Camera.open();
         camera.setDisplayOrientation(90);
 
+        Camera.Parameters parameters = camera.getParameters();
+        parameters.setPreviewFormat(ImageFormat.NV21);
+        camera.setParameters(parameters);
+        // 通过PreviewCallback设置预览回调
+        camera.setPreviewCallback(new Camera.PreviewCallback() {
+            @Override
+            public void onPreviewFrame(byte[] data, Camera camera) {
+                // 处理data
+                // 获取尺寸，在进行转换的时候需要使用
+                mPreviewSize = camera.getParameters().getPreviewSize();
+                //取发YUVIMAGE
+                YuvImage yuvImage = new YuvImage(
+                        data,
+                        ImageFormat.NV21,
+                        mPreviewSize.width,
+                        mPreviewSize.height,
+                        null
+                );
+                // 声明字节输出流
+                ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                //yuvimage 转换成jpg格式
+                yuvImage.compressToJpeg(new Rect(0, 0, mPreviewSize.width, mPreviewSize.height), 100, bos);// 80--JPG图片的质量[0-100],100最高
+                byte[] imageBytes = bos.toByteArray();
+                //将mImageBytes转换成bitmap
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inPreferredConfig = Bitmap.Config.RGB_565;
+
+                Bitmap mBitmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length, options);
+                icon.setImageBitmap(mBitmap);
+            }
+        });
     }
 }
