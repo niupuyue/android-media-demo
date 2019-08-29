c99da4de2431dad8285a8e3c1d1f03408fb086f5
paulniu <niupuyue@aliyun.com>
2019-08-29 15:35:03 +0800

    使用相机绘制一个三角形

--- 06-android-media-opengl/app/src/main/java/com/paulniu/media_opengl/demo03/MyOpenGLRenderViewWithCamera.java
+++ 06-android-media-opengl/app/src/main/java/com/paulniu/media_opengl/demo03/MyOpenGLRenderViewWithCamera.java
@@ -1,7 +1,16 @@
 package com.paulniu.media_opengl.demo03;
 
 import android.content.Context;
+import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
+
+import com.paulniu.media_opengl.R;
+import com.paulniu.media_opengl.demo1.ShaderUtils;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -15,26 +24,71 @@ import javax.microedition.khronos.opengles.GL10;
  */
 public class MyOpenGLRenderViewWithCamera implements GLSurfaceView.Renderer {
 
-    private float triangleCoords[] = {
+    private Context context;
 
+    private int program;
+
+    private int positionHandler;
+    private int colorHandler;
+    private int matrixHandler;
+
+    private float[] viewMatrix = new float[16];
+    private float[] projectMatrix = new float[16];
+    private float[] mvpMatrix = new float[16];
+
+    private float triangleCoords[] = {
+            0.5f, 0.5f, 0.0f, // top
+            -0.5f, -0.5f, 0.0f, // bottom left
+            0.5f, -0.5f, 0.0f  // bottom right
     };
 
-    public MyOpenGLRenderViewWithCamera(Context context){
+    private FloatBuffer vertexBuffer;
 
+    public MyOpenGLRenderViewWithCamera(Context context) {
+        this.context = context;
+        // 申请底层存储控件
+        ByteBuffer bb = ByteBuffer.allocateDirect(triangleCoords.length * 4);
+        bb.order(ByteOrder.nativeOrder());
+        vertexBuffer = bb.asFloatBuffer();
+        vertexBuffer.put(triangleCoords);
+        vertexBuffer.position(0);
     }
 
     @Override
     public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
-
+        String vertexSource = ShaderUtils.readRawText(context, R.raw.vertex_shader_3);
+        String fragmentSource = ShaderUtils.readRawText(context, R.raw.fragment_shader_3);
+        program = ShaderUtils.createProgram(vertexSource, fragmentSource);
+        if (program > 0) {
+            positionHandler = GLES20.glGetAttribLocation(program, "vPosition");
+            colorHandler = GLES20.glGetUniformLocation(program, "af_Color");
+            matrixHandler = GLES20.glGetUniformLocation(program, "vMatrix");
+        }
     }
 
     @Override
-    public void onSurfaceChanged(GL10 gl10, int i, int i1) {
-
+    public void onSurfaceChanged(GL10 gl10, int width, int height) {
+        // 计算宽高比
+        float ratio = (float) width / height;
+        // 设置透视投影
+        Matrix.frustumM(projectMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
+        // 设置相机的位置
+        Matrix.setLookAtM(viewMatrix, 0, 0, 0, 7f, 0f, 0f, 0f, 0f, 1f, 0f);
+        // 计算变换矩阵
+        Matrix.multiplyMM(mvpMatrix, 0, projectMatrix, 0, viewMatrix, 0);
     }
 
     @Override
     public void onDrawFrame(GL10 gl10) {
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        GLES20.glClearColor(0.5f, 0.5f, 0.5f, 1f);
 
+        GLES20.glUseProgram(program);
+        // 设置填充颜色
+        GLES20.glUniform4f(colorHandler, 1f, 1f, 0f, 1f);
+        GLES20.glEnableVertexAttribArray(positionHandler);
+        GLES20.glUniformMatrix4fv(matrixHandler,1,false,mvpMatrix,0);
+        GLES20.glVertexAttribPointer(positionHandler, 3, GLES20.GL_FLOAT, false, 12, vertexBuffer);
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 3);
     }
 }
---
+++ 06-android-media-opengl/app/src/main/res/raw/fragment_shader_3.glsl
@@ -0,0 +1,8 @@
+// 设置我们使用中等精确度
+precision mediump float;
+// 声明变量用于传递颜色数值
+uniform vec4 af_Color;
+void main() {
+    // 将af_Color的颜色属性值赋值给gl_FragColor的内置变量
+    gl_FragColor = af_Color;
+}
